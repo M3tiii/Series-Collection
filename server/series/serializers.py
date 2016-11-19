0954b266d79179e3fa15e32694538aaa63b0af75
M3tiii <m1995g@gmail.com>
2016-11-20 00:27:50 +0100

    Setup server.

---
+++ server/series/serializers.py
@@ -0,0 +1,8 @@
+from series.models import Series
+from rest_framework import serializers
+
+
+class SeriesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Series
+        fields = ('url', 'title')
--- server/series/views.py
+++ server/series/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from series.serializers import SeriesSerializer
+from series.models import Series
 
-# Create your views here.
+
+class SeriesViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
