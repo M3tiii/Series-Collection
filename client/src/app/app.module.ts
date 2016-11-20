010898f1a4d70af18d2e266db5a62d7546d4c9bb
M3tiii <m1995g@gmail.com>
2016-11-20 17:27:08 +0100

    Added series component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { SeriesComponent } from './series/series.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/series/series.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Element, setupElements } from '../services/elements';
+
+@Component({
+  selector: 'app-series',
+  templateUrl: './series.component.html',
+  styleUrls: ['./series.component.css']
+})
+export class SeriesComponent implements OnInit {
+  elements: Element[];
+
+  constructor() {
+    this.elements = [];
+
+    setupElements(this.elements, [
+      { header: "Title", value: "title" },
+      { header: "Website", value: "website" },
+      { header: "Language", value: "language" },
+      { header: "Category", value: "category" },
+    ])
+  }
+
+  ngOnInit() {
+  }
+
+}
