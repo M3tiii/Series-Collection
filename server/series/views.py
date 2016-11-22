5b428356c96baa60936a89951db5e92ca1449228
z61gn13w <z61gn13w@gmail.com>
2016-11-22 22:34:13 +0100

    Added models: Stat, Person, Director

--- server/series/views.py
+++ server/series/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from series.serializers import SeriesSerializer, SeasonSerializer, EpisodeSerializer, AwardSerializer, CompanySerializer
-from series.models import Series, Season, Episode, Award, Company
+from series.serializers import SeriesSerializer, SeasonSerializer, EpisodeSerializer, AwardSerializer, CompanySerializer, StatSerializer, PersonSerializer, DirectorSerializer
+from series.models import Series, Season, Episode, Award, Company, Stat, Person, Director
 
 
 class SeriesViewSet(viewsets.ModelViewSet):
@@ -37,3 +37,24 @@ class CompanyViewSet(viewsets.ModelViewSet):
     """
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+class StatViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Stat.objects.all()
+    serializer_class = StatSerializer
+
+class PersonViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class DirectorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
