fba26673313bd4c35f0f97833190eeb1aa68a896
M3tiii <m1995g@gmail.com>
2016-11-24 00:28:44 +0100

    Fixed config.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,14 +9,20 @@ import { ListComponent } from './list/list.component';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { SeriesComponent } from './series/series.component';
 import { OrderBy } from './pipes/orderBy.pipe';
+import { SeasonComponent } from './season/season.component';
+
+import { DclWrapper } from './dcl-wrapper/dcl-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
+    DclWrapper,
+    SeasonComponent,
     SeriesComponent,
     OrderBy,
   ],
+  entryComponents: [SeasonComponent],
   imports: [
     BrowserModule,
     FormsModule,
