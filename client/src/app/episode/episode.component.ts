f3bb31dbbb154105218807f5ffce64d1e3d10d00
M3tiii <m1995g@gmail.com>
2016-11-25 15:51:19 +0100

    Added episode component.

---
+++ client/src/app/episode/episode.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Element, setupElements } from '../services/elements';
+import { EpisodeService } from '../services/episode.service';
+
+@Component({
+  selector: 'app-episode',
+  templateUrl: './episode.component.html',
+  styleUrls: ['./episode.component.css'],
+  providers: [EpisodeService],
+})
+export class EpisodeComponent implements OnInit {
+  elements: Element[];
+
+  constructor(private service: EpisodeService) {
+    this.elements = [];
+    setupElements(this.elements, [
+      { header: "season", value: "season", isSortable: true },
+      { header: "number", value: "number", isSortable: true },
+      { header: "title", value: "title", isSortable: true },
+      { header: "Release Date", value: "releaseDate", isSortable: true },
+      { header: "Runtime", value: "runtime", isSortable: true },
+    ])
+  }
+
+  listClick(element) {
+    // console.log(element);
+  }
+
+  ngOnInit() { }
+
+}
