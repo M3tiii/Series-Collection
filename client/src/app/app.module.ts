3c27604ee9aa8d10bb3da896e610942f31b89ed8
M3tiii <m1995g@gmail.com>
2016-12-02 19:33:06 +0100

    Added edit-form and navbar dependencies.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { TabsModule, ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
+
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
-
-import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
-import { SeriesComponent } from './series/series.component';
-import { OrderBy } from './pipes/orderBy.pipe';
-import { SeasonComponent } from './season/season.component';
-
 import { DclWrapper } from './dcl-wrapper/dcl-wrapper.component';
+import { SeriesComponent } from './series/series.component';
+import { SeasonComponent } from './season/season.component';
 import { EpisodeComponent } from './episode/episode.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EditFormComponent } from './edit-form/edit-form.component';
+import { OrderBy } from './pipes/orderBy.pipe';
 
 @NgModule({
   declarations: [
@@ -23,13 +24,16 @@ import { EpisodeComponent } from './episode/episode.component';
     SeriesComponent,
     OrderBy,
     EpisodeComponent,
+    NavbarComponent,
+    EditFormComponent,
   ],
   entryComponents: [SeasonComponent, EpisodeComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule
+    ModalModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
