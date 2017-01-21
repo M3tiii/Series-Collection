86e436efedd69065aa917249da9f9210beeb10ff
M3tiii <m1995g@gmail.com>
2017-01-21 01:05:53 +0100

    Fixed pipes.

--- client/src/app/pipes/filter.pipe.ts
+++ client/src/app/pipes/filter.pipe.ts
@@ -5,11 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(value: any, args: string[]): any {
+    // console.log(value, args[0]);
     let filter = args[0].toLocaleLowerCase();
     return filter ? value.filter(object => {
       let passes = false;
       for (let property in object) {
-        if (property != 'options') {
+        if (property != 'options' && !Array.isArray(object[property]) && !(typeof object[property] === "object") && object[property] != null && !Number.isInteger(object[property])) {
+          // console.log('prop', property); //todo fix that
           passes = passes || (object[property].toLocaleLowerCase().indexOf(filter) != -1);
         }
       }
